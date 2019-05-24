b390e8712ca355d04ec91884d4079930abf1963c
Sean Murphy <filmmakersean@fastmail.net>
2019-05-24 01:41:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camebert"]
-  ingredients.each |food| do
+  ingredients.each do |food|
     if cheese_types.include?(food)
       return food
     end
